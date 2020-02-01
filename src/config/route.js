06d89069c74e7f4df87e78e4c0adc0c16cfbc615
kerbal <tranquockhanh997@gmail.com>
2020-02-01 20:06:10 +0700

    update server config

--- src/config/route.js
+++ src/config/route.js
@@ -1,3 +1,3 @@
-export const ENDPOINT = process.env.REACT_APP_MODE === 'DEVELOPMENT' ? 'http://localhost:3001/api/' : '';
+export const ENDPOINT = process.env.REACT_APP_MODE === 'DEVELOPMENT' ? 'http://localhost:3001/api/' : 'https://ledis-api.herokuapp.com/api/';
 
 export const EXE_CMD = ENDPOINT + 'execute?command=';
